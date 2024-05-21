c3f1930597489d9e2e223a918fc87750bdff9f22
finanovita <finanovita479@gmail.com>
2024-05-21 10:32:18 +0700

    Add project files.

---
+++ Initiatization/Initiatization.cpp
@@ -0,0 +1,14 @@
+// Initiatization.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+cl
+
+int main()
+{
+    
+}
+
