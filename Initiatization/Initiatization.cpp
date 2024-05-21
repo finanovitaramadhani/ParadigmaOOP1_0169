56b81d5cddd7246f93107ab01888c04af095f196
finanovita <finanovita479@gmail.com>
2024-05-21 18:17:13 +0700

    Membuat class mahasiswa private dan public pada member initialization

--- Initiatization/Initiatization.cpp
+++ Initiatization/Initiatization.cpp
@@ -5,10 +5,25 @@
 #include <string>
 using namespace std;
 
-cl
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
 
-int main()
-{
-    
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Membuat
+        //Definisi
+    }
+
+    ~mahasiswa() {
+        cout << "Id     = " << id << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << "Nilai  = " << nilai << endl;
+    }
+};
+
+int main() {
+   
 }
 
